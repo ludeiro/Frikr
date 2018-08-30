8668632214978fd35fdc0739ab881641e0470fbd
ludeiro <alberto_ludeiro@hotmail.com>
2018-08-30 19:24:30 +0200

    Login Funcionality

--- users/views.py
+++ users/views.py
@@ -2,11 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import logout as django_logout, authenticate, login as django_login
 
 
 def login(request):
-    pass
+    error_messages = []
+    if request.method == 'POST':
+        username = request.POST.get('usr')
+        password = request.POST.get('pwd')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            error_messages.append('Nombre de usuario o contraseña incorrectos')
+        else:
+            if user.is_active:
+                django_login(request, user)
+                return redirect('photos_home')
+            else:
+                error_messages.append('El usuario no está activo')
+
+    context = {
+        'errors': error_messages
+    }
+    return render(request, 'users/login.html', context)
 
 
 def logout(request):
